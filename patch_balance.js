bab9b37b9b27f00cb41f8e7f69d77062ce4fc117
theghostadmin1 <theghostgame1112@gmail.com>
2026-09-01 04:14:51 +0700

    Xoa cac file rac

--- patch_balance.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = 'e:/TheGhostWeb/index_backup/script.js';
-let content = fs.readFileSync(path, 'utf8');
-content = content.replace(/balance-display/g, 'header-balance');
-fs.writeFileSync(path, content, 'utf8');
-console.log('Fixed balance element ID');
